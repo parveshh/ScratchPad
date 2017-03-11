7fe1806f93c831304e49d983fe8b97cd3403d091
Parvesh <parveshh@gmail.com>
2017-03-11 19:20:48 +0000

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "./services/UserService";
+import { User } from "./models/usermodel";
+import { NgRedux } from "@angular-redux/store";
+import { IAppState } from "./StateStore/Store";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+    selector: "app-root",
+    template: '<router-outlet></router-outlet>',
+    
+    
+
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+    public message: string;
+    private _userService: UserService;
+    constructor(userService: UserService) {
+        this.message = "Hello World";
+        this._userService = userService;
+    }
+
+    ngOnInit() {
+        /* this._userService.createUser({ DisplayName: "Parvesh", EmailAddress: "Parvesh@gmail.com", Password: "123456xx", UserId: "" }).then((user) => {
+
+        //     console.log(user);
+         });*/
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { AngularFireModule } from "angularfire2";
+import { UserService } from "./services/UserService";
+import { NgReduxModule, NgRedux, DevToolsExtension } from "@angular-redux/store";
+import { RouterModule, Router } from '@angular/router';
+import { RouteConfig } from "./app.routes";
+import { LoginComponent } from "./login/login.component";
+import { NavComponent } from "./sharedcomponents/nav.component";
+import { RegisterComponent } from "./register/register.component";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { IAppState, rootReducer, InitialState } from "./StateStore/Store";
+import { userReducer } from "./StateStore/Reducers/UsersReducer";
+import { HomeComponent } from "./home/home.component";
 
-import { AppComponent } from './app.component';
+export const firebaseConfig = {
+    apiKey: "AIzaSyCCE6cIsKJC_4W3ruFMEGMVz93nCXI15iw",
+    authDomain: "logsreader-3a436.firebaseapp.com",
+    databaseURL: "https://logsreader-3a436.firebaseio.com",
+    storageBucket: "logsreader-3a436.appspot.com",
+    messagingSenderId: "1009410504127"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [BrowserModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        NgReduxModule,
+        RouterModule.forRoot(RouteConfig.appRoutes),
+        ReactiveFormsModule,
+        FormsModule
+    ],
+    declarations: [AppComponent, LoginComponent,
+        NavComponent, RegisterComponent, HomeComponent],
+    bootstrap: [AppComponent],
+    providers: [UserService]
+
+
 })
-export class AppModule { }
+
+export class AppModule {
+
+    constructor(ngRedux: NgRedux<IAppState>, devTools: DevToolsExtension) {
+        ngRedux.configureStore(rootReducer, InitialState, [], [devTools.enhancer()]);
+    }
+}
